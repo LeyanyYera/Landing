c37713b331b695ca6cab4c3b73e55ac722581748
leyany <leyany.yera.moya@gmail.com>
2017-11-19 18:07:14 -0300

    revision

--- index.php
+++ index.php
@@ -11,109 +11,99 @@
     <title>Alegria</title>
 </head>
 <body>
-    <div class="row">
-        <div class="col-sm-10 col-sm-offset-1" style="background-color: black">
-            
-            <div class="row" >
-                <div class="col-sm-10 col-sm-offset-1">
-                    <img src="images/Cabezal.png" class="image_cabezal"/>
-                    <div class="col-sm-3 col-sm-offset-1 logo" >
-                        <a href=""><img src="images/Logo.png" id="logo"></a>
+    <div class="container base">
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1 col-xs-12 background_header">
+                <div class="row">
+                    <div class="col-xs-6 text-left">
+                        <a href=""><img src="images/logo.png" class="logo"></a>
                     </div>
-                    <div class="col-sm-2 col-sm-offset-10 bandera">
-                        <a href=""><img src="images/Bandera.png" width="30%" id="bandera"></a>
+                    <div class="col-xs-6 text-right">
+                        <a href=""><img src="images/bandera.png" class="bandera"></a>
                     </div>
-                </div>    
-            </div>
+                </div>
+            </div>    
+        </div>
 
-            <div class="row">
-                <div class="col-sm-12 col-sm-offset-0">
-                    <img src="images/Background.png" class="background"/>
-                    <div class="col-sm-12 text-center central_text">
-                        <img src="images/GET.png" width="35%" id="get" /> 
-                    </div>
-                    <div class="col-sm-2 col-sm-offset-8 text-right roundLogoDiv">
-                        <img src="images/RoundLogo.png" width="155%" id="roundLogo"> 
-                    </div>
-                    <div class="row text-center continue_div">
-                        <button class="continue">PLAY NOW!</button>
-                    </div>
-                    <div class="row text-center new_customer_div">
-                        <a href=""><p class="new_customer"><u>New customers, T&Cs apply.</u></p></a>
-                    </div>
-                    <div class="row text-center register">
-                        <img src="images/1.png" width="60%" id="register"> 
-                    </div>
-                    <div class="row text-center register_next">
-                        <p class="register_next_text">>></p>
-                    </div>
-                    <div class="row text-center deposit">
-                        <img src="images/2.png" width="60%" id="deposit"> 
-                    </div>
-                    <div class="row text-center deposit_next">
-                        <p class="deposit_next_text">>></p>
-                    </div>
-                    <div class="row text-center play">
-                        <img src="images/3.png" width="60%" id="play"> 
-                    </div>
-                </div>  
-            </div>
-
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-1" style="margin-bottom: 15px;">
-                    <img src="images/Rectangle 2.png" width="100%"/>
-                    <div class="container_pay_option">
-                        <span class="pay_option"><img src="images/boleto bancario.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/tansferenciabancaria.png" width="6%"/></span>
-                        <span class="pay_option"><img src="images/astropaycard.png" width="6%"/></span>
-                        <span class="pay_option"><img src="images/astropaydirect.png" width="6%"/></span>
-                        <span class="pay_option"><img src="images/paypal.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/ecopayz.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/paysafecard.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/cubits.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/skrill.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/neteller.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/itau.png" width="3%"/></span>
-                        <span class="pay_option"><img src="images/Banco brasil.png" width="5%"/></span>
-
-                        <span class="pay_option"><img src="images/bradesco.png" width="7%"/></span>
-                        <span class="pay_option"><img src="images/caixabank.png" width="8%"/></span>
-                        <span class="pay_option"><img src="images/santander.png" width="8%"/></span>
-                        <span class="pay_option"><img src="images/jcb.png" width="4%"/></span>
-                        <span class="pay_option"><img src="images/visa.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/mastercard.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/americanexpress.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/neosurf.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/simpler.png" width="5%"/></span>
-                        <span class="pay_option"><img src="images/safetypay.png" width="6%"/></span>
-                        <span class="pay_option"><img src="images/pagoejecutivo.png" width="6%"/></span>
-                    </div>
-                </div>  
-            </div>
-
-            <div class="row">
-                <div class="col-sm-10 col-sm-offset-1">
-                    <div class="footer">
-                        <div class="row">
-                            <div class="col-sm-5 text-center logos_footer_left">
-                                <a href=""><img src="images/Logo.png" class="logo_footer"></a>
-                                <img src="images/Sello oro.png" class="sello_oro">
-                                <img src="images/18mas.png">
-                                <p class="copyright">Copyright C 2017 AlegriaBet.com</p>
-                            </div>
-                            <div class="col-sm-5 col-sm-offset-1 text-right text_footer_container">
-                                <p class="text_footer">Alegriabrt.com is operated by EJAD TECH LTD B.V.
-                                with domicille in Dr. MJ Hugenholtzweg z/n UTS. Gebouw,
-                                Curacao, holder curaçao gaming license no 1668/JAZ</p>                      
-                            </div> 
-                            <div class="col-sm-1 text-center logo_curazao_container">
-                                <a href=""><img src="images/Logo Curacao.png" class="logo_curazao"></a>
-                            </div>  
-                        </div>
-                    </div>           
-                </div>            
+        <div class="row">
+            <div class="col-xs-12 col-sm-10 col-sm-offset-1" style="padding: 0">
+                <img src="images/background_body.png" class="img-responsive background_body"/>
+                <img src="images/GET.png" width="50%" id="get" style="position: absolute; top: 5%; margin-left: 28%"/>
+                <img src="images/RoundLogo.png" width="28%" id="roundLogo" style="position: absolute; top: 35%; margin-left: 72%"> 
+                <button class="continue" style="">PLAY NOW!</button>
+                <a href=""><p class="new_customer"><u>New customers, T&Cs apply.</u></p></a>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-3 col-sm-offset-1" style="padding: 0; background-image: url('images/Rectangle 5.png'); margin-bottom: 10px">
+                <img id="register" src="images/1.png" class="img-responsive" width="35%" style="margin-left: 55%; margin-top: 5%; margin-bottom: 5%" />
+            </div>
+            <div class="col-xs-12 col-sm-1 next_container" style="padding: 0; background-image: url('images/Rectangle 5.png'); margin-bottom: 10px;">
+                <p class="next text-center" style="font-size: 20px; color: #f9d02e; margin: auto; margin-top: 10%; margin-bottom: 20%">></p>
+            </div>
+            <div class="col-xs-12 col-sm-2" style="padding: 0; background-image: url('images/Rectangle 5.png'); margin-bottom: 10px">
+                <img id="deposit" src="images/2.png" class="img-responsive" width="65%" style="margin: auto; margin-top: 5%; margin-bottom: 4%" />
+            </div>
+            <div class="col-xs-12 col-sm-1 next_container" style="padding: 0; background-image: url('images/Rectangle 5.png'); margin-bottom: 10px">
+                <p class="next text-center" style="font-size: 20px; color: #f9d02e; margin: auto; margin-top: 10%; margin-bottom: 20%">></p>
+            </div>
+            <div class="col-xs-12 col-sm-3" style="padding: 0; background-image: url('images/Rectangle 5.png'); margin-bottom: 10px">
+                <img id="play" src="images/3.png" class="img-responsive" width="35%" style="margin-left: 0%; margin-top: 5%; margin-bottom: 2%" />
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-10 col-sm-offset-1" style="padding: 0; background-image: url('images/Rectangle 2.png'); margin-bottom: 10px">
+                <div class="container_pay_option text-center">
+                    <img src="images/boleto bancario.png" width="4%"/>
+                    <img src="images/tansferenciabancaria.png" width="6%"/>
+                    <img src="images/astropaycard.png" width="6%"/>
+                    <img src="images/astropaydirect.png" width="6%"/>
+                    <img src="images/paypal.png" width="6%"/>
+                    <img src="images/ecopayz.png" width="6%"/>
+                    <img src="images/paysafecard.png" width="8%"/>
+                    <img src="images/cubits.png" width="6%"/>
+                    <img src="images/skrill.png" width="4%"/>
+                    <img src="images/neteller.png" width="6%"/>
+                    <img src="images/itau.png" width="3%"/>
+                    <img src="images/Banco brasil.png" width="7%"/>
+
+                    <img src="images/bradesco.png" width="8%"/>
+                    <img src="images/caixabank.png" width="9%"/>
+                    <img src="images/santander.png" width="9%"/>
+                    <img src="images/jcb.png" width="3%"/>
+                    <img src="images/visa.png" width="6%"/>
+                    <img src="images/mastercard.png" width="5%"/>
+                    <img src="images/americanexpress.png" width="6%"/>
+                    <img src="images/neosurf.png" width="5%"/>
+                    <img src="images/simpler.png" width="6%"/>
+                    <img src="images/safetypay.png" width="7%"/>
+                    <img src="images/pagoejecutivo.png" width="6%"/>
+                </div>
+
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1" style="background-color: #00933D">
+                <div class="row">
+                    <div class="col-sm-6 col-xs-12">
+                        <a href="">
+                            <img src="images/Logo.png" class="logo_footer" style="margin-top: 3%; margin-left: 20%; margin-bottom: 3%">
+                        </a>
+                        <img src="images/Sello oro.png" class="sello_oro">
+                        <img src="images/18mas.png" class="mas">
+                        <p class="copyright">Copyright C 2017 AlegriaBet.com</p>
+                    </div>
+                    <div class="col-sm-6 col-xs-12">
+                        <a href=""><img src="images/Logo Curacao.png" class="logo_curazao" style="margin-top: 8%; float: right; margin-right: 5%"/></a>  
+                        <p class="text_footer text-right">Alegriabet.com is operated by EJAD TECH LTD B.V. with domicille in Dr. MJ Hugenholtzweg z/n UTS. Gebouw, Curacao, holder curaçao gaming license no 1668/JAZ</p>
+                    </div>  
+                </div>
+            </div>            
+        </div>
+
     </div>       
 
     <script type="text/javascript" src="js/jquery.min.js"></script>
